b47a0c3086f9c6c68e32697f9eb7aa9e34cac719
abg <gytgg0859@gmail.com>
2025-06-26 22:18:41 +0600

    Create zero.config.js

---
+++ zero.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'zeroBypass',
+  version: '1.1.0',
+  description: 'A plugin for bypassing URLs using Zero API',
+  author: '?',
+  dependencies: {
+    'discord.js': '^14.0.0',
+  },
+  permissions: ['SEND_MESSAGES', 'USE_APPLICATION_COMMANDS'],
+  main: 'zero.plugin.js',
+  commands: [],
+  events: ['chat', 'click_button'],
+};
